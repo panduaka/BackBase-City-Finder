70cedd590d74fc23513255ce5d8d473ecbc91519
pandukawedisinghe <panduka.wedisinghe@gmail.com>
2022-01-30 10:16:58 +0800

    Fix Koin issue

--- app/src/main/java/com/example/backbasecityfinder/CityFinderApplication.kt
+++ app/src/main/java/com/example/backbasecityfinder/CityFinderApplication.kt
@@ -1,16 +1,22 @@
 package com.example.backbasecityfinder
 
 import android.app.Application
+import com.example.backbasecityfinder.di.repositoryModule
+import com.example.backbasecityfinder.di.serviceModule
+import com.example.backbasecityfinder.di.useCaseModule
+import com.example.backbasecityfinder.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class CityFinderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@CityFinderApplication)
+            modules(viewModelModule, repositoryModule, serviceModule, useCaseModule)
         }
     }
 }
