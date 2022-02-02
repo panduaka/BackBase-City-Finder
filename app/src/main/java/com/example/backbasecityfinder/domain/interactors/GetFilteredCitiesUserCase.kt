aad06c761e97f49f4274bff98b88cd31533e66bc
pandukawedisinghe <panduka.wedisinghe@gmail.com>
2022-02-02 10:08:20 +0800

    Add use case business logics to fetch and filter data

--- app/src/main/java/com/example/backbasecityfinder/domain/interactors/GetFilteredCitiesUserCase.kt
+++ app/src/main/java/com/example/backbasecityfinder/domain/interactors/GetFilteredCitiesUserCase.kt
@@ -3,24 +3,25 @@ package com.example.backbasecityfinder.domain.interactors
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.backbasecityfinder.common.Resource
-import com.example.backbasecityfinder.common.UseCase
 import com.example.backbasecityfinder.common.UseCaseScope
+import com.example.backbasecityfinder.common.UseCaseWithParameter
 import com.example.backbasecityfinder.data.remote.dto.City
 import com.example.backbasecityfinder.domain.repository.CityRepository
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 @Suppress("TooGenericExceptionCaught")
-class GetCitiesUserCase(private val cityRepository: CityRepository) :
-    UseCase<LiveData<Resource<List<City>>>>,
+class GetFilteredCitiesUserCase(private val cityRepository: CityRepository) :
+    UseCaseWithParameter<String, LiveData<Resource<List<City>>>>,
     UseCaseScope {
-    override fun execute(): LiveData<Resource<List<City>>> {
+    override fun execute(parameter: String): LiveData<Resource<List<City>>> {
         val result = MutableLiveData<Resource<List<City>>>()
         result.postValue(Resource.Loading())
         launch {
             try {
-                val cities = cityRepository.getCities()
-                result.postValue(Resource.Success(cities.sortedBy { it.name }))
+                val cities =
+                    filterList(searchText = parameter, cities = cityRepository.getFetchedCities())
+                result.postValue(Resource.Success(cities))
             } catch (exception: Exception) {
                 result.postValue(Resource.Error(exception))
             }
@@ -30,5 +31,13 @@ class GetCitiesUserCase(private val cityRepository: CityRepository) :
 
     override fun cancel() {
         coroutineContext.cancel()
+    }
+
+    private fun filterList(searchText: String, cities: List<City>): List<City> {
+        return cities.filter {
+            it.name.startsWith(searchText)
+        }.sortedBy {
+            it.name
+        }
     }
 }
