ac0088fafe44e693cc658ae022f07a93dab3a9c7
pandukawedisinghe <panduka.wedisinghe@gmail.com>
2022-02-02 22:09:43 +0800

    Add internet permission, repository type casting issue,URL formatiing

--- app/src/main/java/com/example/backbasecityfinder/data/remote/HttpRoutes.kt
+++ app/src/main/java/com/example/backbasecityfinder/data/remote/HttpRoutes.kt
@@ -1,6 +1,6 @@
 package com.example.backbasecityfinder.data.remote
 
 object HttpRoutes {
-    private const val BASE_URL = "https://panduka-test-app.s3.ap-southeast-1.amazonaws.com/BackBase/cities.json"
-    const val POSTS = "$BASE_URL"
+    private const val BASE_URL = "https://panduka-test-app.s3.ap-southeast-1.amazonaws.com"
+    const val POSTS = "$BASE_URL/BackBase/cities.json"
 }
--- app/src/main/java/com/example/backbasecityfinder/di/RepositoryModule.kt
+++ app/src/main/java/com/example/backbasecityfinder/di/RepositoryModule.kt
@@ -2,10 +2,11 @@ package com.example.backbasecityfinder.di
 
 import com.example.backbasecityfinder.data.remote.Service
 import com.example.backbasecityfinder.data.remote.repository.CityRepositoryImpl
+import com.example.backbasecityfinder.domain.repository.CityRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { CityRepositoryImpl(get()) }
+    single { CityRepositoryImpl(get()) as CityRepository}
 }
 
 val serviceModule = module {
